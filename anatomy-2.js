cd27480058b4610ec9d1f360b7ef5fe56d964b3c
pauloWeslleY <weslleyspfc13@outlook.com>
2022-07-14 02:52:50 -0300

    aula #02 anatomy-02

---
+++ anatomy-2.js
@@ -0,0 +1,21 @@
+//! Anonymous Function
+(function (a, b, c) {
+   return a + b + c;
+})
+
+//! Function Expression
+const sum = function (a, b) {
+   return a + b;
+}
+
+const result = sum(7, 56);
+console.log(result);
+
+const anotherSum = sum;
+console.log(anotherSum(5, 6));
+
+let x = 3;
+console.log(x);
+
+x = sum;
+console.log(x(11, 26));
