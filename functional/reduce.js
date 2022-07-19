fee915e851c57296563b9d15c6f78ef4e7cd590e
pauloWeslleY <weslleyspfc13@outlook.com>
2022-07-19 18:46:41 -0300

    aula #14 method reduce 2

--- functional/reduce.js
+++ functional/reduce.js
@@ -1,6 +1,16 @@
 const numbers = [1, 2, 3, 4, 5, 6];
 
 const sum = (all, el) => all + el
-const calc = numbers.reduce(sum);
-
+const calc = numbers.reduce(sum, 1350);
 console.log(calc);
+
+const average = (acc, el, i, array) => {
+   if (i === array.length - 1) {
+      return (acc + el) / array.length
+   } else {
+      return acc + el
+   }
+}
+
+const result = numbers.reduce(average);
+console.log(result);
