004855693608b763f4b7935b5a94b7b275aada92
pauloWeslleY <weslleyspfc13@outlook.com>
2022-08-16 20:34:46 -0300

    aula #16

--- rx_js/function_1.js
+++ rx_js/function_1.js
@@ -1,1 +1,33 @@
-console.log('Test!');
+// console.log('Test!');
+
+let a = 4;
+console.log(a);
+
+//! Function Declaration
+function goodMorning() {
+   console.log('Good Morning!!');
+}
+
+goodMorning();
+
+//* Function Expression
+const goodAfternoon = function () {
+   console.log('Good Afternoon!!');
+}
+
+goodAfternoon(); //? undefined
+
+
+//? Function
+function calc(a, b = 0) {
+   return a + b;
+}
+
+let result = calc(2, 56);
+console.log(result);
+
+result = calc(4, 6, 5, 67, 87, 45);
+console.log(result);
+
+result = calc(4);
+console.log(result);
