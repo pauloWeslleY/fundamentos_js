4eb58dba3f7aeec5804e9234b36b0165aca79edb
pauloWeslleY <weslleyspfc@13outlook.com>
2022-09-30 20:25:40 -0300

    aula #27

--- rx_js/promise.js
+++ rx_js/promise.js
@@ -16,4 +16,49 @@ new Promise(function (resolve) {
 //? Promise #02
 setTimeout(function () {
    console.log('Running callback...')
+
+   setTimeout(function () {
+      console.log('Running!')
+
+      setTimeout(function () {
+         console.log('Running third!')
+      }, 2000)
+   }, 2000)
 }, 2000)
+
+function isWaitFor(time = 2000) {
+   return new Promise(function (res) {
+      setTimeout(function () {
+         console.log('Running!!!')
+         res()
+      }, time)
+   })
+}
+
+isWaitFor(3000).then(() => isWaitFor()).then(isWaitFor)
+
+
+
+//? Promise #03
+function isGenerate(min, max) {
+   if (min > max) [max, min] = [min, max]
+
+   return new Promise(resolve => {
+      const calculator = max - min + 1
+      const random = parseInt(Math.random() * calculator) + min
+      resolve(random)
+   })
+}
+
+isGenerate(1, 75)
+   .then(num => num * 10)
+   .then(isNumX10 => `this number generate was ${isNumX10}`)
+   .then(console.log)
+
+
+
+
+
+
+
+
